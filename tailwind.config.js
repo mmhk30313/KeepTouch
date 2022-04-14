a6041a960073c7c2d1ec5ebd2c099d5ac00872b8
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2022-04-14 17:47:19 +0600

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const tailwindcss = require('tailwindcss');
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer')
+  ],
 }
